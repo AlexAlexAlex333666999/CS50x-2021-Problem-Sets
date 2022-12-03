3006c79f6a2ae2fb69564b6989f6122db652c279
Alexander Moroz <47663039+AlexAlexAlex333666999@users.noreply.github.com>
2022-12-03 14:08:50 +0300

    Update volume.c

--- volume.c
+++ volume.c
@@ -11,25 +11,25 @@ int main(int argc, char *argv[])
 {
     // Check command-line arguments
     if (argc != 4)
-      {
+    {
         printf("Usage: ./volume input.wav output.wav factor\n");
         return 1;
-      }
+    }
 
     // Open files and determine scaling factor
     FILE *input = fopen(argv[1], "r");
     if (input == NULL)
-      {
+    {
         printf("Could not open file.\n");
         return 1;
-      }
+    }
 
     FILE *output = fopen(argv[2], "w");
     if (output == NULL)
-      {
+    {
         printf("Could not open file.\n");
         return 1;
-      }
+    }
 
     float factor = atof(argv[3]);
 
@@ -43,11 +43,11 @@ int main(int argc, char *argv[])
     int16_t buffer;
 
     while (fread(&buffer, sizeof(buffer), 1, input))
-      {
+    {
         buffer = buffer * factor;
         fwrite(&buffer, sizeof(buffer), 1, output);
         buffer = buffer / factor;
-      }
+    }
 
     // Close files
     fclose(input);
